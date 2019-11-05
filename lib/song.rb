5852bedc9d44c45483805707e163709b08e5259f
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-05 22:12:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,13 @@ class Song
     song
   end
   
-  def self.create_from_filename
+  def self.create_from_filename(filename)
+    song = self.new
+    song.name = (filename.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (filename.split(" - ")[0])
+    @@all << song
+    song
+  end
   
   
   
